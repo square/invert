094568fc288fd433b393193d49dede229d131949
Sam Edwards <samedwards@squareup.com>
2024-05-15 21:09:36 -0400

    kotlin("multiplatform") -> kotlin("jvm") to publish just JAR files.

--- collectors-anvil-dagger/build.gradle.kts
+++ collectors-anvil-dagger/build.gradle.kts
@@ -1,30 +1,20 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
+    id("java-gradle-plugin")
     kotlin("plugin.serialization")
     alias(libs.plugins.vanniktech.maven.publish)
 }
 
-kotlin {
-    jvm {
-        withSourcesJar()
-    }
+java {
+    withSourcesJar()
+}
 
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                api(libs.kotlinx.serialization.core)
-                api(libs.kotlinx.serialization.json)
-                implementation(libs.kotlin.compiler.embeddable)
-                implementation(project(":invert-models"))
-                implementation(project(":invert-gradle-plugin"))
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
-    }
+dependencies {
+    api(libs.kotlinx.serialization.core)
+    api(libs.kotlinx.serialization.json)
+    implementation(libs.kotlin.compiler.embeddable)
+    implementation(project(":invert-models"))
+    implementation(project(":invert-gradle-plugin"))
+
+    testImplementation(libs.kotlin.test)
 }
