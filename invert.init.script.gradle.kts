db6108f0af0cc1e52b0501f1f4d5eb08045aed04
Sam Edwards <samedwards@squareup.com>
2024-05-22 14:45:55 +0200

    Added GitHub Codeowners back to the init script

--- invert.init.script.gradle.kts
+++ invert.init.script.gradle.kts
@@ -43,7 +43,7 @@ class InvertInitScriptPlugin : Plugin<Gradle> {
                         classpath("com.squareup.invert:invert-gradle-plugin:$invertVersion")
                         classpath("com.squareup.invert:collectors-anvil-dagger:$invertVersion")
                         classpath("com.squareup.invert:collectors-kotlin-java-loc:$invertVersion")
-//                        classpath("com.squareup.invert:owners-github-codeowners:$invertVersion")
+                        classpath("com.squareup.invert:owners-github-codeowners:$invertVersion")
                     }
                 }
             }
