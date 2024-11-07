a2527adcc9621ebeeaf36a14aaa56997ce61c09d
Sam Edwards <samedwards@squareup.com>
2024-11-07 11:01:52 -0500

    Removed Dependency Injection and Hotwire Pages from the "common" report as they do not make sense here. WIP add java di collector
    Added Jacob's new collector.
    
    Put it in the "Burndown" category.
    
    Using in Register and Cash Android
    
    Added Tests

--- invert-gradle-plugin/src/main/kotlin/com/squareup/invert/CollectedStat.kt
+++ invert-gradle-plugin/src/main/kotlin/com/squareup/invert/CollectedStat.kt
@@ -2,11 +2,13 @@ package com.squareup.invert
 
 import com.squareup.invert.models.Stat
 import com.squareup.invert.models.StatMetadata
+import kotlinx.serialization.Serializable
 
 
 /**
  * A Collected [Stat] and its associated [StatMetadata]
  */
+@Serializable
 data class CollectedStat(
     val metadata: StatMetadata,
     val stat: Stat?
