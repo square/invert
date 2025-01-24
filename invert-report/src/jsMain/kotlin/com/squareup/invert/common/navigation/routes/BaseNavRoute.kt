4f8245394140085914d3df3396a412600e4e9aa7
Sam Edwards <samedwards@squareup.com>
2025-01-24 13:10:41 -0500

    Added a `toQueryString()` for creating links.

--- invert-report/src/jsMain/kotlin/com/squareup/invert/common/navigation/routes/BaseNavRoute.kt
+++ invert-report/src/jsMain/kotlin/com/squareup/invert/common/navigation/routes/BaseNavRoute.kt
@@ -5,20 +5,24 @@ import com.squareup.invert.common.navigation.NavPageId
 import com.squareup.invert.common.navigation.NavRoute
 
 abstract class BaseNavRoute(
-    final override val navPage: NavPage,
+  final override val navPage: NavPage,
 ) : NavRoute {
-    override fun toSearchParams(): Map<String, String> = toParamsWithOnlyPageId(this)
+  override fun toSearchParams(): Map<String, String> = toParamsWithOnlyPageId(this)
 
-    override val page: NavPageId = navPage.pageId
+  override val page: NavPageId = navPage.pageId
 
-    companion object {
+  companion object {
 
-        const val PAGE_ID_PARAM = "page"
+    const val PAGE_ID_PARAM = "page"
 
-        fun toParamsWithOnlyPageId(navRoute: NavRoute): MutableMap<String, String> {
-            return mutableMapOf<String, String>().apply {
-                this[PAGE_ID_PARAM] = navRoute.page
-            }
-        }
+    fun toParamsWithOnlyPageId(navRoute: NavRoute): MutableMap<String, String> {
+      return mutableMapOf<String, String>().apply {
+        this[PAGE_ID_PARAM] = navRoute.page
+      }
     }
+  }
+
+  fun toQueryString(): String {
+    return toSearchParams().map { (key, value) -> "$key=$value" }.joinToString("&")
+  }
 }
