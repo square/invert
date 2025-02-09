1d8b2897ff96122f311117f7f81c8a1b4247305b
Sam Edwards <samedwards@squareup.com>
2025-02-08 20:48:23 -0500

    Using same constant for Unowned.

--- invert-gradle-plugin/src/main/kotlin/com/squareup/invert/internal/NoOpInvertOwnershipCollector.kt
+++ invert-gradle-plugin/src/main/kotlin/com/squareup/invert/internal/NoOpInvertOwnershipCollector.kt
@@ -2,6 +2,7 @@ package com.squareup.invert.internal
 
 import com.squareup.invert.InvertOwnershipCollector
 import com.squareup.invert.models.ModulePath
+import com.squareup.invert.models.OwnerInfo
 import com.squareup.invert.models.OwnerName
 import java.io.File
 
@@ -12,5 +13,5 @@ object NoOpInvertOwnershipCollector : InvertOwnershipCollector {
   override fun collect(
     gitRootDir: File,
     fileWithOwnership: File
-  ): OwnerName = "NoOp Invert Ownership"
+  ): OwnerName = OwnerInfo.UNOWNED
 }
