1516758f35a00d005499ecb12cd00379b7569288
Sam Edwards <samedwards@squareup.com>
2025-03-11 17:19:35 -0500

    Added new (optional) markdown string "documentation" field for a Stat. (#47)

--- invert-models/src/commonMain/kotlin/com/squareup/invert/models/Typealiases.kt
+++ invert-models/src/commonMain/kotlin/com/squareup/invert/models/Typealiases.kt
@@ -3,6 +3,7 @@ package com.squareup.invert.models
 typealias DependencyId = String
 typealias ConfigurationName = String
 typealias GradlePluginId = String
+typealias Markdown = String
 typealias ModulePath = String
 typealias FileKey = String
 typealias OwnerName = String
