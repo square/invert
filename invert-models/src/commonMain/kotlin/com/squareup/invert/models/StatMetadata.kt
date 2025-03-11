870b7e6ca17394f3e35efcad81bd8d2dd9aaf2f6
Sam Edwards <samedwards@squareup.com>
2025-03-11 15:09:15 -0700

    Added new (optional) markdown string "documentation" field for a Stat.

--- invert-models/src/commonMain/kotlin/com/squareup/invert/models/StatMetadata.kt
+++ invert-models/src/commonMain/kotlin/com/squareup/invert/models/StatMetadata.kt
@@ -8,9 +8,16 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class StatMetadata(
+  /** The unique key for this [Stat]. */
   val key: StatKey,
+  /** A short, human-readable description of this [Stat]. Use [documentation] field for detailed info. */
   val description: String,
+  /** The data type of this [Stat]. */
   val dataType: StatDataType,
+  /** This is used for grouping [Stat] by category. */
   val category: String = "Stats",
+  /** Metadata for the registered key/value "extras" for this [Stat]. */
   val extras: List<ExtraMetadata> = emptyList(),
+  /** Documentation on this [Stat] in Markdown format. */
+  val documentation: Markdown? = null,
 )
