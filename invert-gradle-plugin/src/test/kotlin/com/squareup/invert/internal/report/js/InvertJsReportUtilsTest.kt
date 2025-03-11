c24512df7ca0b7c209202305244d063ea10e8846
Sam Edwards <samedwards@squareup.com>
2025-03-11 10:49:18 -0700

    Fix test to use the new API

--- invert-gradle-plugin/src/test/kotlin/com/squareup/invert/internal/report/js/InvertJsReportUtilsTest.kt
+++ invert-gradle-plugin/src/test/kotlin/com/squareup/invert/internal/report/js/InvertJsReportUtilsTest.kt
@@ -89,7 +89,7 @@ class InvertJsReportUtilsTest {
     "code_reference_stat": {
         "metadata": {
             "key": "code_reference_stat",
-            "description": "Code Reference Stat",
+            "title": "Code Reference Stat",
             "dataType": "CODE_REFERENCES"
         },
         "total": 6,
@@ -101,7 +101,7 @@ class InvertJsReportUtilsTest {
     "numeric_stat": {
         "metadata": {
             "key": "numeric_stat",
-            "description": "Numeric Stat",
+            "title": "Numeric Stat",
             "dataType": "NUMERIC"
         },
         "total": 62,
