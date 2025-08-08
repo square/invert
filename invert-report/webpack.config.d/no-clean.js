404042750096b78b73c8a49903084b18741de9ef
Sam Edwards <samedwards@squareup.com>
2025-08-07 22:10:12 -0400

    Custom config for the webpack task to not clear the output directory before copying the resulting files.

--- invert-report/webpack.config.d/no-clean.js
+++ invert-report/webpack.config.d/no-clean.js
@@ -1,5 +1,8 @@
+// Custom webpack config
+// https://kotlinlang.org/docs/js-project-setup.html#custom-webpack-configuration
+
 // This prevents the webpack output from clearing/cleaning the directory first
-// Without this the other files are deleted.
+// This matches the behavior from Kotlin 2.0.0.
 config.output = {
   ...config.output,
   clean: false
